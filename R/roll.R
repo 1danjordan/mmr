3162f18175522d3f909aa492b3192efe0dd9c4c6
Dan Jordan <dandermotj@gmail.com>
2016-09-25 22:32:45 +0100

    roll docs

--- R/roll.R
+++ R/roll.R
@@ -1,11 +1,19 @@
-#' Rolling Window .fction
+#' Rolling Window function
 #'
-#' Roll the window across a series of data, applying a .fction.
-#' Use \code{purrr::partial} to partially fill a .fction with arguments
-#' before passing it into \code{roll}.
+#' A simple roll function, "rolling the window" across a series of data,
+#' applying a function, to produce a vector of results
 #'
-#' @param x       a vector/matrix passed into \code{.f}
-#' @param .f      a partial function to pass x into
+#' @param x       a vector passed into \code{.f}
+#' @param .f      a function or formula
+#'
+#'   If a \strong{function}, it is used as is.
+#'
+#'   If a \strong{formula}, e.g. \code{~ .x + 2}, it is converted to a
+#'   function with two arguments, \code{.x} or \code{.} and \code{.y}. This
+#'   allows you to create very compact anonymous functions with up to
+#'   two inputs.
+#'
+#' @param ...     further arguments to pass .f
 #' @param width   the width of the window
 #' @param extend  extend the window as it rolls (logical)
 #'
@@ -13,8 +21,7 @@
 #' # Simulate 4 years of daily returns
 #' x <- rnorm(1000)
 #' # compute the historical var across a window of a year
-#' var_h <- partial(var_historical, conf = 0.8) %>%
-#' roll(x, var_h, 250)
+#' roll(x, var_historical, conf = 0.8, width = 250)
 
 roll <- function(x, .f, ..., width = 10, extend = FALSE) {
 
