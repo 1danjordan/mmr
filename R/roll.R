063ad67b068d908e52190bdb914984fb3a0bd24a
Dan Jordan <dandermotj@gmail.com>
2016-09-25 22:08:47 +0100

    working roll

--- R/roll.R
+++ R/roll.R
@@ -1,13 +1,13 @@
-#' Rolling Window function
+#' Rolling Window .fction
 #'
-#' Roll the window across a series of data, applying a function.
-#' Use \code{purrr::partial} to partially fill a function with arguments
+#' Roll the window across a series of data, applying a .fction.
+#' Use \code{purrr::partial} to partially fill a .fction with arguments
 #' before passing it into \code{roll}.
 #'
-#' @param x        a vector/matrix passed into \code{fun}
-#' @param fun      a partial function to pass x into
-#' @param length   the length of the window
-#' @param extend   extend the window as it rolls (logical)
+#' @param x       a vector/matrix passed into \code{.f}
+#' @param .f      a partial function to pass x into
+#' @param width   the width of the window
+#' @param extend  extend the window as it rolls (logical)
 #'
 #' @example
 #' # Simulate 4 years of daily returns
@@ -16,12 +16,18 @@
 #' var_h <- partial(var_historical, conf = 0.8) %>%
 #' roll(x, var_h, 250)
 
-roll <- function(x, fun, length, extend = FALSE) {
+roll <- function(x, .f, ..., width = 10, extend = FALSE) {
 
-  idxs <- ifelse(extend,
-                 map(seq(1, length(x) - length - 1), ~ seq(1, .x + length)),
-                 map(seq(1, length(x) - length - 1), ~ seq(.x, .x + length)))
+  .f <- as_function(.f, ...)
 
-  map_dbl(idxs, ~ fun(x[.x]))
+  idxs <- if(extend) {
+    map(seq(1, length(x) - width + 1), ~ seq(1, .x + width - 1))
+  } else {
+    map(seq(1, length(x) - width + 1), ~ seq(.x, .x + width - 1))
+
+  }
+
+  map_dbl(idxs, ~ .f(x[.x]))
+
 
 }
